f5fa7e32e8394e8a2a34fefa842b7c7b35cd438a
CM Ngan <plusonecm@gmail.com>
2024-03-29 19:00:28 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,163 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+
+const isDev = false;
+// const isDev = true;
+
+type roleT = 'thief' | 'normal' | 'thief-cover' | 'bomb';
+const roleMap: Record<roleT, string> = {
+	thief: '👑',
+	normal: '💭',
+	'thief-cover': '🖐️',
+	bomb: '💥',
+};
+
+type diceT = 1 | 2 | 3 | 4 | 5 | 6;
+const dV = (d: number) => (d >= 1 && d <= 6 ? `⏰ ${d}` : '⏰');
+type playerT = {
+	name: string;
+	role: roleT;
+	dice: diceT;
+	visible: boolean;
+};
+
+type storeT = {
+	players: playerT[];
+	stage: 'setup' | 'game' | 'end';
+	remarks: string;
+};
+
+const initialStore: storeT = {
+	players: [],
+	stage: 'setup',
+	remarks: '',
+};
+
+const preSet: Array<{
+	name: string;
+	pplNum: number;
+	hasBomb: boolean;
+}> = [
+	{ name: '4P 👑', pplNum: 4, hasBomb: false },
+	{ name: '5P 👑 🖐️\n🖐️⏰ = 👑⏰', pplNum: 5, hasBomb: false },
+	{ name: '6P 👑 🖐️\n🖐️⏰ = 7⏰', pplNum: 6, hasBomb: false },
+	{ name: '6P 👑 🖐️ 💥\n🖐️⏰ = 7⏰', pplNum: 6, hasBomb: true },
+	{ name: '7P 👑 🖐️2\n🖐️⏰ = 7⏰\n🙈👑', pplNum: 7, hasBomb: false },
+	{ name: '7P 👑 🖐️2 💥\n🖐️⏰ = 7⏰\n🙈👑', pplNum: 7, hasBomb: true },
+	{ name: '8P 👑 🖐️2\n🖐️⏰ = 7⏰', pplNum: 8, hasBomb: false },
+	{ name: '8P 👑 🖐️2 💥\n🖐️⏰ = 7⏰', pplNum: 8, hasBomb: true },
+];
+
+const StageSetup = (props: { setStore: (s: storeT) => void }) => {
+	const onClick = (p: (typeof preSet)[number]) => {
+		const players: playerT[] = [];
+
+		const thiefIndex = Math.floor(Math.random() * p.pplNum);
+		const _bombIndex = p.hasBomb
+			? Math.floor(Math.random() * (p.pplNum - 1))
+			: -1;
+		const bombIndex = _bombIndex >= thiefIndex ? _bombIndex + 1 : _bombIndex;
+
+		for (let i = 0; i < p.pplNum; i++) {
+			const dice = (Math.floor(Math.random() * 6) + 1) as diceT;
+			const playerName =
+				(!isDev && window.prompt(`Player ${i + 1} name:`)) || `Player ${i + 1}`;
+			const role: roleT =
+				i === thiefIndex ? 'thief' : i === bombIndex ? 'bomb' : 'normal';
+			!isDev && window.alert(`${playerName} is ${roleMap[role]} (${dV(dice)})`);
+			players.push({
+				name: playerName,
+				role,
+				dice,
+				visible: false,
+			});
+		}
+
+		console.log(players);
+		props.setStore({ players, stage: 'game', remarks: p.name });
+	};
+
+	return (
+		<div className="flex flex-col justify-center h-full">
+			<h1 className="text-center font-bold text-3xl animate-bounce">
+				Banana Thief
+			</h1>
+			<div className="grid grid-cols-2 gap-4 mt-8">
+				{preSet.map((p) => (
+					<button
+						key={p.name}
+						onClick={() => onClick(p)}
+						className="border border-blue-400 rounded-lg p-8 whitespace-pre-wrap leading-8"
+					>
+						{p.name}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const StageGame = (props: { store: storeT; setStore: (s: storeT) => void }) => {
+	const { store, setStore } = props;
+	const [isEnd, setIsEnd] = useState(false);
+
+	return (
+		<>
+			<h2 className="whitespace-pre-wrap text-xl text-center leading-10">
+				{store.remarks}
+			</h2>
+			<div className="flex flex-col justify-center flex-1">
+				<div className="grid grid-cols-2 gap-8">
+					{store.players.map((player, i) => {
+						let txt = player.name;
+						if (isEnd)
+							txt = `${txt}\n${roleMap[player.role]}\n${dV(player.dice)}`;
+						else if (player.visible) txt = `${txt}\n${dV(player.dice)}`;
+
+						return (
+							<button
+								key={i}
+								className="border border-blue-400 rounded-lg py-10 px-2 whitespace-pre-wrap"
+								onClick={() => {
+									setStore({
+										...store,
+										players: store.players.map((p, j) =>
+											i === j ? { ...p, visible: !p.visible } : p
+										),
+									});
+								}}
+							>
+								{txt}
+							</button>
+						);
+					})}
+				</div>
+			</div>
+			<div className="flex gap-4 justify-end">
+				<button
+					className="text-2xl mx-2"
+					onClick={() => setStore(initialStore)}
+				>
+					🆕
+				</button>
+				<button className="text-2xl mx-2" onClick={() => setIsEnd(!isEnd)}>
+					👁️
+				</button>
+			</div>
+		</>
+	);
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [store, setStore] = useState<storeT>(initialStore);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col p-4 inset-0 absolute">
+			{store.stage === 'setup' && <StageSetup setStore={setStore} />}
+			{store.stage === 'game' && (
+				<StageGame store={store} setStore={setStore} />
+			)}
+		</main>
+	);
 }
